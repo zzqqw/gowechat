db7ff33542fa29a784b55373ab07069c23b68ef6
zzqqw <zhiqiangwang@aliyun.com>
2022-11-17 23:18:27 +0800

    1

--- work/batch.go
+++ work/batch.go
@@ -11,9 +11,13 @@ func NewBatch(work *WechatWork) *Batch {
 	return &b
 }
 
+type JobId struct {
+	JobId string `json:"jobid"`
+}
+
 type SyncResp struct {
 	client.BaseResp
-	JobId string `json:"jobid"`
+	JobId
 }
 type SyncUser struct {
 	MediaID  string       `json:"media_id"`
@@ -58,6 +62,36 @@ type ReplaceParty struct {
 func (b *Batch) ReplaceParty(req ReplaceParty) (SyncResp, error) {
 	var resp SyncResp
 	err := b.work.GetClient(ContactClientName).HttpPostJsonAssign("/cgi-bin/tag/replaceparty", req, &resp)
+	if err != nil {
+		return resp, err
+	}
+	return resp, nil
+}
+
+type SyncGetResult struct {
+	client.BaseResp
+	Status     int          `json:"status"`
+	Type       string       `json:"type"`
+	Total      int          `json:"total"`
+	Percentage int          `json:"percentage"`
+	Result     []SyncResult `json:"result"`
+}
+type SyncResult []struct {
+	client.BaseResp
+	Userid string `json:"userid"`
+}
+
+type SyncGetResultResp struct {
+	client.BaseResp
+	JobId
+}
+
+// GetResult 获取异步任务结果
+// https://developer.work.weixin.qq.com/document/path/90983
+func (b *Batch) GetResult(jobId string) (SyncGetResult, error) {
+	var resp SyncGetResult
+	err := b.work.GetClient(ContactClientName).
+		HttpGetAssign("/cgi-bin/tag/getresult", JobId{JobId: jobId}, &resp)
 	if err != nil {
 		return resp, err
 	}
